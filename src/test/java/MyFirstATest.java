0decad7c18d8f42a17f8f4db005ebfb3ed7fb499
Acer <vagus12@gmail.com>
2022-04-21 22:24:12 +0300

    resolved

--- src/test/java/MyFirstATest.java
+++ src/test/java/MyFirstATest.java
@@ -1,25 +1,35 @@
-//import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MyFirstATest {
-//    @Test
-//    public void openPage() {
-////        Selenide.open("http://automationpractice.com/index.php");
-//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Acer\\Downloads\\geckodriver-v0.31.0-win64\\geckodriver.exe");
-//        WebDriver driver = new FirefoxDriver();
-//        driver.get("http://automationpractice.com/index.php");
-//    }
+    @Test
+    public void openPage() {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Acer\\Downloads\\geckodriver-v0.31.0-win64\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://automationpractice.com/index.php");
+        driver.close();
+    }
+
     @Test
     public void openPage2() {
-//        Selenide.open("http://automationpractice.com/index.php");
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Acer\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://automationpractice.com/index.php");
+        driver.close();
+    }
 
+    @Test
+    public void openPage3() {
+        Selenide.open("http://automationpractice.com/index.php");
+    }
+
+    @Test
+    public void openPage4() {
+        System.setProperty("selenide.browser", "firefox");
+        Selenide.open("http://automationpractice.com/index.php");
     }
 
     public static void main(String[] args) {
