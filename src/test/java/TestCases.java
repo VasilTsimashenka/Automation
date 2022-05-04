cf7b98787eb9548781aaf77691c4e5d2aef2c608
Acer <vagus12@gmail.com>
2022-05-04 21:16:57 +0300

    created testCases 1 and 2

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -10,7 +10,20 @@ public class TestCases {
     public void testCase1() {
         Selenide.open("http://automationpractice.com/index.php");
         $(".login").click();
-        sleep(5000);
+        $("#email").sendKeys("tests891@gmail.com");
+        $("#passwd").sendKeys("12345");
+        $("#SubmitLogin").click();
+        $("#block_top_menu > ul > li:nth-child(2) > a").hover();
+        $("#block_top_menu > ul > li:nth-child(2) > ul > li:nth-child(1) > a").click();
+        $("#center_column > ul > li > div > div.right-block > h5 > a").scrollIntoView(true).hover();
+        $(".ajax_add_to_cart_button").click();
+        $("div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a").click();
+        $(" .standard-checkout.button-medium").click();
+        $("#center_column > form > p > button").click();
+        $("#uniform-cgv").click();
+        $("#form > p > button").click();
+        $("#HOOK_PAYMENT > div:nth-child(1) > div > p > a").click();
+        $("#cart_navigation > button").click();
 
     }
 
@@ -18,46 +31,56 @@ public class TestCases {
     public void testcase2() {
         Selenide.open("http://automationpractice.com/index.php");
         $(".login").click();
-        sleep(2000);
         $("#email_create").sendKeys(RandomStringUtils.randomAlphabetic(5) + "@gmail.com");
-        sleep(2000);
         $("#SubmitCreate").click();
-        sleep(2000);
         $("#uniform-id_gender1").click();
-        sleep(1000);
         $("#customer_firstname").sendKeys("Vasil");
         $("#customer_lastname").sendKeys("Tim");
         $("#passwd").sendKeys("12345");
         $("#uniform-days").click();
         $("#days").sendKeys("1");
-        sleep(2000);
         $("#uniform-months").click();
         $("#months > option:nth-child(2)").click();
-        sleep(2000);
         $("#uniform-years").click();
         $("#years").sendKeys("2020");
-        sleep(2000);
         $("#uniform-newsletter").click();
         $("#uniform-optin").click();
-        sleep(1000);
         $("#company").sendKeys("Kaseya");
         $("#address1").sendKeys("701 Brickell Avenue");
-        sleep(3000);
         $("#city").sendKeys("Miami");
         $("#uniform-id_state").click();
         $("#id_state").sendKeys("Florida");
-        sleep(2000);
         $("#postcode").sendKeys("33131");
         $("#uniform-id_country").click();
         $("#id_country").sendKeys("United States)");
-        sleep(1000);
         $("#other").sendKeys("Additional information");
         $("#phone").sendKeys("+375292929297");
         $("#phone_mobile").sendKeys("+375333333333");
         $("#alias").sendKeys("18, Baker street");
-        sleep(5000);
         $("#submitAccount").click();
-        sleep(2000);
+        $("#block_top_menu > ul > li:nth-child(2) > a").hover();
+        $("#block_top_menu > ul > li:nth-child(2) > ul > li:nth-child(1) > a").click();
+        $("#center_column > ul > li > div > div.right-block > h5 > a").scrollIntoView(true).hover();
+        $(".ajax_add_to_cart_button").click();
+        $("div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a").click();
+        $(" .standard-checkout.button-medium").click();
+        $("#center_column > form > p > button").click();
+        $("#uniform-cgv").click();
+        $("#form > p > button").click();
+        $("#HOOK_PAYMENT > div:nth-child(1) > div > p > a").click();
+        $("#cart_navigation > button").click();
+        $("#contact-link > a").click();
+        $("#id_contact").sendKeys("Customer service");
+
+        $("div.clearfix > div.col-xs-12.col-md-3 > div:nth-child(6) > div").click();
+        $("div:nth-child(6) > div > select > option:nth-child(2)").click();
+
+        $("#message").sendKeys("I have a problem with my order. Could you help me?");
+
+        $("#submitMessage").click();
+        sleep(4000);
+
+
 
     }
 }
