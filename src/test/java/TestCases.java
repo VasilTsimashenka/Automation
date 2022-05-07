fe5115676659f3fea0e4fd9afdd773193d33500e
Acer <vagus12@gmail.com>
2022-05-07 11:59:43 +0300

    created testCases 1 and 2

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.apache.commons.lang.RandomStringUtils;
 import org.junit.jupiter.api.Test;
@@ -60,7 +61,7 @@ public class TestCases {
         $("#submitAccount").click();
         $("#block_top_menu > ul > li:nth-child(2) > a").hover();
         $("#block_top_menu > ul > li:nth-child(2) > ul > li:nth-child(1) > a").click();
-        $("#center_column > ul > li > div > div.right-block > h5 > a").scrollIntoView(true).hover();
+        $("#center_column > ul > li > div > div.right-block > h5 > a").hover();
         $(".ajax_add_to_cart_button").click();
         $("div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a").click();
         $(" .standard-checkout.button-medium").click();
@@ -73,6 +74,8 @@ public class TestCases {
         $("#id_contact").sendKeys("Customer service");
         $("div.clearfix > div.col-xs-12.col-md-3 > div:nth-child(6) > div").click();
         $("div:nth-child(6) > div > select > option:nth-child(2)").click();
+        String orderId = $("div.selector [name=\"id_order\"").getValue();
+        $("select[name=\"id_product\"][id=\"" + orderId + "_order_products\"]").selectOption(1);
         $("#message").sendKeys("I have a problem with my order. Could you help me?");
         $("#submitMessage").click();
     }
