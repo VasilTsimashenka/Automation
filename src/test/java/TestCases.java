c3d3cd83d9c2fef96f9cad3f3de9ad38631642f3
Acer <vagus12@gmail.com>
2022-05-23 18:28:47 +0300

    Revert "My 1 attempе"
    This reverts commit b2a8b909598cbd87832b3a52129a14ba9529e6bc.

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,33 +1,16 @@
-package tests;
-
-import base.BaseTest;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.apache.commons.lang.RandomStringUtils;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import pages.CreateAccountPage;
-import pages.MainPage;
-import pages.SignInPage;
-import service.Proceed_Checkout;
 
-import static com.codeborne.selenide.Condition.disappear;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static pages.CreateAccountPage.createAccount;
-import static pages.MainPage.*;
-import static service.Proceed_Checkout.clickConfirmOrderPaymentPageBtn;
-import static service.Proceed_Checkout.proceedCheckoutPrintedDress;
-import static utils.CreateRandomEmail.mailRandom;
 
-public class TestCases extends BaseTest {
-    MainPage mainPage = new MainPage();
-    Proceed_Checkout proceedCheckout = new Proceed_Checkout();
-
-
+public class TestCases {
     @Test
     public void orderPaymentOfGoods() {
-        open("/");
+        Selenide.open("http://automationpractice.com/index.php");
         $(".login").click();
         $("#email").sendKeys("tests891@gmail.com");
         $("#passwd").sendKeys("12345");
@@ -46,34 +29,56 @@ public class TestCases extends BaseTest {
 
     }
 
-
     @Test
     public void sendEmailToCustomerService() {
-
-
-//        MainPage.clickSignInButton();
-//        SignInPage.loginAs("", "");
-//        CreateAccountPage.createAccount("", "", "");
-//        Assertions.assertEquals();
-        open("/");
-        clickSignInButton();
-        mailRandom(5);
-        createAccount("Vasil", "Tim", "12345");
-        selectCasualDresses();
-        addCartPrintedDress();
-        proceedCheckoutPrintedDress();
-        clickConfirmOrderPaymentPageBtn();
-        customerServiceContactUsPage();
-
-//        $("#contact-link > a").click();
-//        $("#id_contact").sendKeys("Customer service");
-//        $(".col-md-3 > div:nth-child(6) > div > select").click();
-//        $("div:nth-child(6) > div > select > option:nth-child(2)").click();
-//        String orderId = $("div.selector [name=\"id_order\"").getValue();
-//        $("select[name=\"id_product\"][id=\"" + orderId + "_order_products\"]").selectOption(1);
-//        $("#message").sendKeys("I have a problem with my order. Could you help me?");
-//        $("#submitMessage").click();
-
+        Selenide.open("http://automationpractice.com/index.php");
+        $(".login").click();
+        $("#email_create").sendKeys(RandomStringUtils.randomAlphabetic(5) + "@gmail.com");
+        $("#SubmitCreate").click();
+        $("#uniform-id_gender1").click();
+        $("#customer_firstname").sendKeys("Vasil");
+        $("#customer_lastname").sendKeys("Tim");
+        $("#passwd").sendKeys("12345");
+        $("#uniform-days").click();
+        $("#days").sendKeys("1");
+        $("#uniform-months").click();
+        $("#months > option:nth-child(2)").click();
+        $("#uniform-years").click();
+        $("#years").sendKeys("2020");
+        $("#uniform-newsletter").click();
+        $("#uniform-optin").click();
+        $("#company").sendKeys("Kaseya");
+        $("#address1").sendKeys("701 Brickell Avenue");
+        $("#city").sendKeys("Miami");
+        $("#uniform-id_state").click();
+        $("#id_state").sendKeys("Florida");
+        $("#postcode").sendKeys("33131");
+        $("#uniform-id_country").click();
+        $("#id_country").sendKeys("United States)");
+        $("#other").sendKeys("Additional information");
+        $("#phone").sendKeys("+375292929297");
+        $("#phone_mobile").sendKeys("+375333333333");
+        $("#alias").sendKeys("18, Baker street");
+        $("#submitAccount").click();
+        $("#block_top_menu > ul > li:nth-child(2) > a").hover();
+        $("#block_top_menu > ul > li:nth-child(2) > ul > li:nth-child(1) > a").click();
+        $("#center_column > ul > li > div > div.right-block > h5 > a").hover();
+        $(".ajax_add_to_cart_button").click();
+        $("div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a").click();
+        $(" .standard-checkout.button-medium").click();
+        $("#center_column > form > p > button").click();
+        $("#uniform-cgv").click();
+        $("#form > p > button").click();
+        $("#HOOK_PAYMENT > div:nth-child(1) > div > p > a").click();
+        $("#cart_navigation > button").click();
+        $("#contact-link > a").click();
+        $("#id_contact").sendKeys("Customer service");
+        $("div.clearfix > div.col-xs-12.col-md-3 > div:nth-child(6) > div").click();
+        $("div:nth-child(6) > div > select > option:nth-child(2)").click();
+        String orderId = $("div.selector [name=\"id_order\"").getValue();
+        $("select[name=\"id_product\"][id=\"" + orderId + "_order_products\"]").selectOption(1);
+        $("#message").sendKeys("I have a problem with my order. Could you help me?");
+        $("#submitMessage").click();
     }
 
     @Test
@@ -92,7 +97,7 @@ public class TestCases extends BaseTest {
         $("#content").sendKeys("Faded short sleeve t-shirt with high neckline. Soft and " +
                 "stretchy material for a comfortable fit. Accessorize with a straw hat and you're ready for summer!");
         $("#submitNewMessage").click();
-        $("#id_new_comment_form").should(disappear);
+        $("#id_new_comment_form").should(Condition.disappear);
 
         assertEquals(
                 "New comment\n" +
@@ -121,14 +126,11 @@ public class TestCases extends BaseTest {
         $(".icon-heart").click();
         $("td:nth-child(1) > a").click();
         $(".wishlist_delete > a").click();
-        Selenide.switchTo().alert().accept();
-        $("#block-history > table").should(disappear);
-        Assertions.assertFalse($("#block-history > table").isDisplayed());
 
     }
 
     @Test
-    public void checkColorSelection() {
+    public void checkColorSelection () {
         Selenide.open("http://automationpractice.com/index.php");
         $("#block_top_menu > ul > li:nth-child(2) > a").hover();
         $("#block_top_menu > ul > li:nth-child(2) > ul > li:nth-child(3) > a").click();
